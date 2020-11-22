9be6bf96b9f15990d3a053edb17d7df6a1bbae5d
code36u4r60 <code36u4r60@hotmail.com>
2020-11-22 00:23:43 +0000

    Capítulo - Upload de Imagens

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,13 @@ import cors from 'cors';
 import routes from './routes';
 
 import './database';
+import uploadConfig from './config/upload';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use('/files', express.static(uploadConfig.directory));
+
 app.use(routes);
 
 app.listen(3333, () => {
